d3cb4b3d638ac615cfb8e0962df52577b2544d50
Sameer-0077 <sameeryadav0077@gmail.com>
2025-08-05 00:58:20 +0530

    fix: add authentication middleware to current user route

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,10 +4,15 @@ const {
   registerUser,
   loginUser,
   logoutUser,
+  getCurrentUser,
 } = require("../controllers/authController");
 const validateAuthUser = require("../middlewares/validateAuthUser");
+const isAuthenticated = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 router.post("/signup", validateNewUser, registerUser);
 router.post("/login", validateAuthUser, loginUser);
 router.get("/logout", logoutUser);
+router.get("/current-user", isAuthenticated, getCurrentUser);
+
+module.exports = router;
