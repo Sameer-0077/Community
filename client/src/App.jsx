95f6cc20baa05509ee0be9eeda0a56acd29a3e8c
Sameer-0077 <sameeryadav0077@gmail.com>
2025-08-04 11:42:29 +0530

    feat: add routing for Home, Profile, Login, and Register pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,24 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/NavBar";
 import Layout from "./Layout";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
-    <div className="bg-gray-900 w-full min-h-screen">
+    <div className="bg-gray-800 min-h-screen">
       {/* <h2 className="p-2 bg-blue-500 text-3xl text-white">Community</h2> */}
 
       <Router>
         <Routes>
-          <Route path="/" element={<Layout />}></Route>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Register />} />
+          </Route>
         </Routes>
       </Router>
     </div>
