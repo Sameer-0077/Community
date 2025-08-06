0a713e588d6e0a93dfd999db0ef72625dd13550d
Sameer-0077 <sameeryadav0077@gmail.com>
2025-08-06 13:35:56 +0530

    feat: implement user fetching logic in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
 import "./App.css";
 import Navbar from "./components/NavBar";
 import Layout from "./Layout";
@@ -6,8 +7,36 @@ import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import useUserStore from "./store/userStore";
 
 function App() {
+  const setUser = useUserStore((state) => state.setUser);
+
+  const getCurrentUser = async () => {
+    try {
+      const res = await fetch("http://localhost:8000/api/auth/current-user", {
+        method: "GET",
+        credentials: "include",
+      });
+
+      const user = await res.json();
+
+      if (!res.ok) {
+        // console.log("Error", user);
+        throw new Error(user.error);
+      } else {
+        console.log("User:", user);
+        setUser(user.user);
+      }
+    } catch (error) {
+      console.log("Error:", error.message);
+    }
+  };
+
+  useEffect(() => {
+    getCurrentUser();
+  }, []);
+
   return (
     <div className="bg-gray-800 min-h-screen">
       {/* <h2 className="p-2 bg-blue-500 text-3xl text-white">Community</h2> */}
