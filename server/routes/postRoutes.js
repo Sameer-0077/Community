fc6f2726a43c474e270ffdf40beda237bc4ef29b
Sameer-0077 <sameeryadav0077@gmail.com>
2025-08-06 13:37:14 +0530

    fix: ensure user authentication for fetching user-specific posts

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -5,11 +5,10 @@ const {
   getAllPosts,
   getUserPosts,
 } = require("../controllers/postController");
-const { route } = require("./userRoutes");
 const router = express.Router();
 
 router.post("/create-post", isAuthenticated, createPost);
 router.get("/all-post", getAllPosts);
-router.get("/user/:userId", getUserPosts);
+router.get("/user/:userId", isAuthenticated, getUserPosts);
 
 module.exports = router;
