cf3ffc80263ac601b4cca40be4ab1f4b22457aa7
Sameer-0077 <sameeryadav0077@gmail.com>
2025-12-28 14:18:47 +0530

    fix: Update CORS origin to use environment variable for better configuration

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const postRoutes = require("./routes/postRoutes");
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
